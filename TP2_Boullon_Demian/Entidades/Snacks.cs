aaf2bf090e11379e8a77121da1062123f89f5023
Demian Boullon <dboullon@q4tech.com>
2017-10-10 16:06:59 -0300

    Tp 02 de Laboatorio, subido tarde.

---
+++ TP2_Boullon_Demian/Entidades/Snacks.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Snacks : Producto
+    {
+        public Snacks(EMarca marca, string patente, ConsoleColor color)
+            : base(marca, patente, color)
+        { }
+
+        /// <summary>
+        /// Los snacks tienen 104 calorías
+        /// </summary>
+        public override short CantidadCalorias
+        {
+            get
+            {
+                return 104;
+            }
+        }
+
+        /// <summary>
+        /// Devuelve la informacion completa de los snacks.
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SNACKS");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine(String.Format("CALORIAS : {0}", this.CantidadCalorias));
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
